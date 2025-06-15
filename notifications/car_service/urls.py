c84702aafa820f1833c4c1674e69c397575c2f48
Aleksander Zizevskikh <zizevskikh.dev@gmail.com>
2025-06-15 22:57:27 +0300

    added black v.25.1.0 to dependencies requirements.txt

--- notifications/car_service/urls.py
+++ notifications/car_service/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from car_service.views import car_service_home_page, parts
+from car_service.views import (
+    car_service_home_page,
+    parts_by_int,
+    parts_by_slug,
+)
 
 
 urlpatterns = [
-    path('', car_service_home_page),
-    path('parts/<int:part_id>/', parts),
+    path("", car_service_home_page),
+    path("parts-int-test/<int:part_int_id>/", parts_by_int),
+    path("parts-slug-test/<slug:part_slug>/", parts_by_slug),
 ]
--- notifications/car_service/views.py
+++ notifications/car_service/views.py
@@ -5,5 +5,9 @@ def car_service_home_page(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Car service home page content")
 
 
-def parts(request: HttpRequest, part_id: int) -> HttpResponse:
-    return HttpResponse(f"<h1>Запчасти</h1><p>Запчасть #{part_id}</p>")
+def parts_by_int(request: HttpRequest, part_int_id: int) -> HttpResponse:
+    return HttpResponse(f"<h1>Запчасти</h1><p>Запчасть #{part_int_id}</p>")
+
+
+def parts_by_slug(request: HttpRequest, part_slug: str) -> HttpResponse:
+    return HttpResponse(f"<h1>Запчасти</h1><p>Запчасть #{part_slug}</p>")
