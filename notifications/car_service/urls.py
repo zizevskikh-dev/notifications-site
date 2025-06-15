19af162cced429a0359631810c9097d9bd77aa58
Aleksander Zizevskikh <zizevskikh.dev@gmail.com>
2025-06-15 19:09:02 +0300

    added dynamic URLs logic for id car parts pages

--- notifications/car_service/urls.py
+++ notifications/car_service/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from car_service.views import car_service_home_page
+from car_service.views import car_service_home_page, parts
 
 
 urlpatterns = [
     path('', car_service_home_page),
+    path('parts/<int:part_id>/', parts),
 ]
--- notifications/car_service/views.py
+++ notifications/car_service/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse, HttpRequest
 
 def car_service_home_page(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Car service home page content")
+
+
+def parts(request: HttpRequest, part_id: int) -> HttpResponse:
+    return HttpResponse(f"<h1>Запчасти</h1><p>Запчасть #{part_id}</p>")
