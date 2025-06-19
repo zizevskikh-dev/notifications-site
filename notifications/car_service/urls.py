67ca0d8d5fd2398dcbf359311974e602330b0503
Aleksander Zizevskikh <zizevskikh.dev@gmail.com>
2025-06-20 01:34:20 +0300

    added base template in 'car-service/' home page

--- notifications/car_service/urls.py
+++ notifications/car_service/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from car_service.views import car_service_homepage
 
-
 urlpatterns = [
     path("", car_service_homepage, name='car_service'),
 ]
--- notifications/car_service/views.py
+++ notifications/car_service/views.py
@@ -6,4 +6,4 @@ def car_service_homepage(request: HttpRequest) -> HttpResponse:
     return render(
         request=request,
         template_name="car_service/car_service.html"
-)
+    )
