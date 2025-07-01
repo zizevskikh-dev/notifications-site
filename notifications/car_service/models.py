5976d1465a093706021b94e3dbf4a6804c27aa4a
Aleksander Zizevskikh <zizevskikh.dev@gmail.com>
2025-07-01 14:57:52 +0300

    added model Parts

--- notifications/car_service/models.py
+++ notifications/car_service/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Parts(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    installation_date = models.DateField(auto_now_add=False, auto_now=False)
+    description = models.TextField(blank=True)
